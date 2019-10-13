e82d0fb10360041a4ee5f59f16aefce4819aedea
xingchen.lin <xingchen.lin@ele.me>
2019-10-13 23:35:54 +0800

    continue practise

---
+++ NLP/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace NLP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.序号 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.分词 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.词性 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.基本词 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listView1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(363, 441);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "词法分析/依存句法分析/DNN语言模型";
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.序号,
+            this.分词,
+            this.词性,
+            this.基本词});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(7, 81);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(350, 129);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // 序号
+            // 
+            this.序号.Text = "序号";
+            // 
+            // 分词
+            // 
+            this.分词.Text = "分词";
+            // 
+            // 词性
+            // 
+            this.词性.Text = "词性";
+            // 
+            // 基本词
+            // 
+            this.基本词.Text = "基本词";
+            this.基本词.Width = 120;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(282, 48);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "词法分析";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(7, 21);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(350, 21);
+            this.textBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(1055, 513);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader 序号;
+        private System.Windows.Forms.ColumnHeader 分词;
+        private System.Windows.Forms.ColumnHeader 词性;
+        private System.Windows.Forms.ColumnHeader 基本词;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ NLP/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Baidu.Aip.Nlp;
+using Newtonsoft.Json.Linq;
+
+namespace NLP
+{
+    public partial class Form1 : Form
+    {
+        String APPID = "17515731";
+        String APIKEY = "IjRnPBB3XkrzSfrTlbDE0B09";
+        String SECRETKEY = "jGuvliw6bkV8EmPddtGawELtEld0Tnif";
+        Nlp nlp;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Nlp getClient()
+        {
+            if (this.nlp != null)
+            {
+                return this.nlp;
+            }
+            this.nlp = new Nlp(APIKEY, SECRETKEY);
+            this.nlp.Timeout = 60000;
+            return this.nlp;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            String cifa = this.textBox1.Text;
+            var result = this.getClient().Lexer(cifa);
+            int num = 0;
+            listView1.Items.Clear();
+            foreach (JObject retObj in result.Value<JArray>("items"))
+            {
+                listView1.Items.Add(num.ToString());
+                listView1.Items[num].SubItems.Add(retObj.Value<string>("item"));
+                listView1.Items[num].SubItems.Add(retObj.Value<string>("pos"));
+                listView1.Items[num].SubItems.Add(retObj.Value<JArray>("basic_words").ToString());
+                num++;              
+            }
+            
+
+        }
+    }
+}
